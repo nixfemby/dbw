0853ac2dcda2beaec0fae9305488d10eb3a43a73
Luna <ayowxifu@tutanota.com>
2022-07-27 15:44:42 +0200

    Add pricing and features page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
-
 import vue from "@astrojs/vue";
+import react from "@astrojs/react";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +15,7 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  }), sitemap({}), vue(), compress()],
+  }), sitemap({}), vue(), compress(), react(), svelte()],
   vite: {
     ssr: {
       external: ["svgo"]
